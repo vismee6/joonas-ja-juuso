90c5b8cb3f9f6dddf052ffca775fe263d9b276ec
vismee6 <38341988+vismee6@users.noreply.github.com>
2018-04-27 13:11:46 +0300

    Lisätty SQLiteV3

---
+++ SQLite_scriptV3.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+sqlite file = "valo_ja_lampo.sqlite"
+table_name1 = "poyta1"
+new_field = "alue1"
+field_type = "INTEGER"
+
+conn = sqlite3.connect(sqlite_file)
+c = conn.cursor()
+
+c.execute("CREATE TABLE {tn} ({nf} {ft})"/
+    .format(tn = table_name1, nf = new_field, ft = field_type))
+
+c.execute("CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)"/
+    .format(tn = table_name2, nf = new_field, ft = field_type))
+
+    conn.commit()
+    conn.close()
