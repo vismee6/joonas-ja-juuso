7fa20add743f83e423cf33774adf613665e8e287
vismee6 <38341988+vismee6@users.noreply.github.com>
2018-05-18 08:46:14 +0300

    xd

---
+++ Pythonprint/jaiks.py
@@ -0,0 +1,12 @@
+import cgi
+gayseks ="""<html>
+    <head>
+        <title>TOIMIIIII</title>
+    </head>
+    <body>
+        <h1>Tassa on tekstia</h1>
+        <p>Tassa on lisaa tekstia</p>
+    </body>
+</html>"""
+
+print(gayseks)
